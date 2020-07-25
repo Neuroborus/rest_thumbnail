87251bb636cfdf625891d1c7f5c78694ec6e1c59
Mikhail <mihasso@protonmail.com>
2020-07-25 21:26:51 +0300

    outlines done

--- src/main.rs
+++ src/main.rs
@@ -3,52 +3,92 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::http::RawStr;
-use rocket::response::content::Content;
-use rocket::http::ContentType;
-use std::fs::File;
-//use std::io::Cursor;
-//use bytes::Bytes;
+use rocket::response::content;
+use rocket::request;
+use rocket::Outcome;
+use rocket::http::{RawStr, Status, ContentType};
+
+#[macro_use]
+extern crate curl;
+use curl::easy;
+
 use image::{DynamicImage, load_from_memory};
 
+#[derive(Debug)]
+enum ApiKeyError {
+    BadCount,
+    Missing,
+}
+
+struct ApiKey(String);
+impl<'a, 'r> request::FromRequest<'a, 'r> for ApiKey {
+    type Error = ApiKeyError;
+
+    fn from_request(request: &'a request::Request<'r>) -> request::Outcome<Self, Self::Error> {
+        let keys: Vec<_> = request.headers().get("url").collect();
+        match keys.len(){
+            0 => Outcome::Failure((Status::BadRequest, ApiKeyError::Missing)),
+            1=> Outcome::Success(ApiKey(keys[0].to_string())),
+            _=> Outcome::Failure((Status::BadRequest, ApiKeyError::BadCount)),
+        }
+
+    }
+}
+
 #[tokio::main]
-async fn get_image(url: &RawStr) -> DynamicImage{
-    let im = reqwest::get(url.as_str())
+async fn get_image(url: String) -> DynamicImage{
+    /*let im = reqwest::get(url.as_str())
         .await.unwrap()
         .bytes()
-        .await.unwrap();
-    load_from_memory(&im.slice(..)).unwrap()
+        .await.unwrap();*/
+    let mut data = Vec::new();
+    let mut handle = easy::Easy::new();
+    handle.url(url.as_str()).unwrap();
+    {
+        let mut transfer = handle.transfer();
+        transfer.write_function(|new_data| {
+            data.extend_from_slice(new_data);
+            Ok(new_data.len())
+        }).unwrap();
+        transfer.perform().unwrap();
+    }
+
+    load_from_memory(&data[..]).unwrap()
 }
 
 #[tokio::main]
 async fn miniature(img: &DynamicImage, width: u32) -> DynamicImage{
-    let (w, h) = img.as_rgba8().unwrap().dimensions();
+    let (w, h) = img.as_rgb8().unwrap().dimensions();
     let del = w/width;
     img.thumbnail(width, h/del)
 }
 
-//file
-/*#[get("/<url>")]
-fn retrieve(url: &RawStr) -> Option<File>{
-    let pic = miniature(&get_image(&url), 100);
-    pic.save("tmp.png").unwrap();
-    File::open("tmp.png").ok()
+/*#[get("/check")]
+fn retrieve(key: ApiKey) -> String{
+    String::from(key.0)
 }*/
 
 //memory
-#[get("/<url>")]
-fn retrieve(url: &RawStr) -> Option<Content<Vec<u8>>>{
+#[get("/img")]
+fn retrieve(key: ApiKey) -> content::Content<Vec<u8>>{
     let mut buf = Vec::new();
-    miniature(&get_image(&url), 100).
+    miniature(&get_image(key.0), 100).
         write_to(&mut buf, image::ImageOutputFormat::PNG).
             unwrap();
+    content::Content(ContentType::PNG, buf)
+}
 
-    let v = Content(ContentType::PNG, buf);
-    Some(v)
+#[get("/<t>")]
+fn index(t: &RawStr) -> String {
+    let mut s = String::from("Hello, ");
+    s.push_str(t.as_str());
+    s.push_str(".!.");
+    
+    s
 }
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes![retrieve])
+        .mount("/", routes![index, retrieve])
         .launch();
 }
